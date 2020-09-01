45ccc34855b1c11cd6ea38d7a4d6c4e9a07ad71f
David Weatherspoon <david_weatherspoon@hotmail.com>
2020-09-01 10:09:30 +0100

    Modification to admin view with clearer horizontal filter applied for cake catergory assignment.

--- cakes/admin.py
+++ cakes/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
 from cakes.models import Catergory, Cake
 
+# Register your models here.
+class CakeAdmin(admin.ModelAdmin):
+    filter_horizontal = ("catergories",)
+
 admin.site.register(Catergory)
-admin.site.register(Cake)
+admin.site.register(Cake, CakeAdmin)
--- elc/settings.py
+++ elc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '5d2c19b9b58bbe30420a17b6fd01f666c205cd9c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
