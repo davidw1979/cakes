bcdf012644c44034684aa71e59292e35ce004087
David Weatherspoon <david_weatherspoon@hotmail.com>
2020-09-02 23:21:09 +0100

    Added routes and templates for contact, product & cookies pages. Logic added via passed parameters to render navigation bars and main backgrounds differently if hero section present. Contact card image added to contact page.

--- cakes/urls.py
+++ cakes/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'cakes'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('products/', views.products, name='products'),
+    path('cookies/', views.cookies, name='cookies'),
 ]
--- cakes/views.py
+++ cakes/views.py
@@ -3,6 +3,23 @@ from cakes.models import Catergory, Cake
 
 # Create your views here.
 def index(request):
-    return render(request, 'cakes/index.html', {
-        "cakes": Cake.objects.all()
-    })
+    context = {'hero_present': 'hero-present'}
+    return render(request, 'cakes/index.html', context)
+
+
+def contact(request):
+    context = {'no_hero': 'no-hero'}
+    return render(request, 'cakes/contact.html', context)
+
+
+def products(request):
+    context = {
+        'no_hero': 'no-hero',
+        'cakes': Cake.objects.all()
+        }
+    return render(request, 'cakes/products.html', context)
+
+
+def cookies(request):
+    context = {'no_hero': 'no-hero'}
+    return render(request, 'cakes/cookies.html', context)
